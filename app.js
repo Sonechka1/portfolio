fba80c866b43ae19debee5d9642bd7da5ca5bf35
Sonechka1 <151186030+Sonechka1@users.noreply.github.com>
2025-01-06 19:30:34 +0300

    new_portfolio

--- app.js
+++ app.js
@@ -9,3 +9,43 @@ workLink.addEventListener('click', function(e){
     }
     workLink.classList.add('hidden')
 })
+
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+      // удалим у кнопки класс btn-up_hide
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      // добавим к кнопке класс btn-up_hide
+      this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+      // при прокрутке содержимого страницы
+      window.addEventListener('scroll', () => {
+        // определяем величину прокрутки
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      // при нажатии на кнопку .btn-up
+      document.querySelector('.btn-up').onclick = () => {
+        // переместим в начало страницы
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  
+  btnUp.addEventListener();
+
+  const educationBtn = document.querySelector('.education-btn');
+  const educationBlock = document.querySelector('.education');
+
+  educationBtn.addEventListener('click', function(){
+    educationBlock.classList.toggle('hidden');
+    educationBtn.classList.toggle('active');
+  })
